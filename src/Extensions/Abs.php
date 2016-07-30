861a611d78092dd701b8372e8e647d23e4fa3594
Sujin <sujin.2f@gmail.com>
2016-07-30 17:36:53 -0400

    get / set error fix

--- src/Extensions/Abs.php
+++ src/Extensions/Abs.php
@@ -29,12 +29,4 @@ abstract class Abs {
 		$this->name = ( !func_num_args() ) ? $this->defaultName : func_get_arg(0);
 		$this->key = sanitize_title( $this->name );
 	}
-
-	public function __set( $name, $value ) {
-		$this->{$name} = $value;
-	}
-
-	public function __get( $name ) {
-		return $this->{$name};
-	}
 }
