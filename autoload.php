f4180ffdf62d6b3b3857146693da5e4d40d16c8a
Sujin <sujin.2f@gmail.com>
2016-09-22 18:36:01 -0400

    Version Controll

--- autoload.php
+++ autoload.php
@@ -1,24 +1,39 @@
 <?php
 /**
- *
  * WP Express Autoloader & Redirect
  *
- * @author	Sujin 수진 Choi
- * @package	wp-express
- * @version	4.5.0
- * @website	http://sujinc.com
+ * @author  Sujin 수진 Choi
+ * @package wp-express
+ * @version 5.0
+ * @website http://sujinc.com
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice
  *
  */
 
-if ( !function_exists( 'loadWordpressExpress' ) ) {
-	function loadWordpressExpress() {
+global $WP_Express_Version;
+
+if ( empty( $WP_Express_Version ) )
+	$WP_Express_Version = array();
+
+$WP_Express_Version[] = 5.0;
+
+add_action( 'after_setup_theme', 'load_wordpress_express_5_0' );
+
+if ( !function_exists( 'load_wordpress_express_5_0' ) ) {
+	function load_wordpress_express_5_0() {
+		// Check the latest version
+		global $WP_Express_Version;
+		arsort( $WP_Express_Version );
+
+		if ( $WP_Express_Version[0] != 5.0 )
+			return;
+
 		spl_autoload_register( function( $className ) {
 			$namespace = 'WE\\';
 			if ( stripos( $className, $namespace ) === false ) {
-		        	return;
+				return;
 			}
 
 			$sourceDir = __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
@@ -28,13 +43,11 @@ if ( !function_exists( 'loadWordpressExpress' ) ) {
 				include $fileName;
 			}
 		});
+
+		new WE_Redirect_5_0;
 	}
 
-	loadWordpressExpress();
-}
-
-if ( !class_exists('WE_Redirect' ) ) {
-	class WE_Redirect {
+	class WE_Redirect_5_0 {
 		public function __construct() {
 			add_filter( 'wp_redirect', array( $this, 'wp_redirect' ) );
 		}
@@ -51,7 +64,5 @@ if ( !class_exists('WE_Redirect' ) ) {
 
 			return $location;
 		}
-
 	}
-	new WE_Redirect;
 }
