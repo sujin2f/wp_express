e1ae0d314528bd70037f6437665f5b9f49204025
Sujin <sujin.2f@gmail.com>
2019-12-05 09:18:09 -0500

    To Typescript

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   collectCoverageFrom: [
-    'assets/**',
+    '<rootDir>/assets/*.ts',
+    "!<rootDir>/assets/**/*.spec.ts",
+    "!<rootDir>/assets/**/__*__/*",
   ],
   verbose: true,
   collectCoverage: true,
   coverageDirectory: "<rootDir>/logs",
   coverageReporters: ["clover"],
+  modulePathIgnorePatterns: [
+      "__mocks__"
+  ],
 };
